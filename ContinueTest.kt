bbd8978b2e26df7ed7a233c3d40dac17bfbb1376
neeraj-maran <86770077+neeraj-maran@users.noreply.github.com>
2021-07-27 17:27:55 +0530

    Continue test
    Kotlin code to test continue statement

---
+++ ContinueTest.kt
@@ -0,0 +1,20 @@
+
+
+fun main()
+{
+    var a = 10
+    printRes(a)
+   /*
+    for (i in 1..20) {
+        if (i>=10 && i<12)
+            continue
+        println("Hello $i")
+        }  */
+}
+fun printRes(a : Int)
+{
+    var b = a + 10
+    println("$a")
+    println("$b")
+
+}
